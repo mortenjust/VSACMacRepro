57f9cd3c808d60eaa78c73d7f36bca6a7b77fef8
fareast\sarawa <sarawa@microsoft.com>
2020-12-04 18:16:30 +0530

    Initial Commit

---
+++ MacAppDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacAppDemo
+//
+//  Created by App Center on 04/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
